91b7e7dab57a0cc2704285b30e5c3d06da8d3b75
0kkama <mydeadtime@gmail.com>
2021-09-14 01:26:00 +0300

    MenuAssembler class created

--- App/classes/utility/MenuAssembler.php
+++ App/classes/utility/MenuAssembler.php
@@ -2,7 +2,36 @@
 
     namespace App\classes\utility;
 
+
+    use App\classes\models\Navigation;
+    use App\classes\models\User;
+    use App\classes\utility\containers\NavigationBar;
+
+    /**
+     * Assembly and returns a collection of menu items depending on the user's rights
+     */
     class MenuAssembler
     {
+        protected NavigationBar $menu;
+        protected User $user;
+        protected array $array;
+        protected static array $build = [
+            'noname'   => ['noname', 'main'],
+            'user'     => ['user', 'main'],
+            'moder'    => ['user', 'admin', 'main'],
+            'admin'    => ['user', 'admin', 'main'],
+            'overseer' => ['user', 'admin', 'main'],
+        ];
 
+        public static function assemblyMenu(User $user): array
+        {
+            $rights = array_flip(Config::getInstance()->getAllRights());
+            if ($user->exist()) {
+                $userR = $user->getRights();
+                $userS = $rights[$userR];
+            } else {
+                $userS = 'noname';
+            }
+            return Navigation::getAllInBy('status', self::$build[$userS], 'order');
+        }
     }
