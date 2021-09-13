76ebf016ae9d586c12ed29fefa06157025d4d362
0kkama <mydeadtime@gmail.com>
2021-09-14 01:23:40 +0300

    Inspectors structure changed

--- App/interfaces/InspectorInterface.php
+++ App/interfaces/InspectorInterface.php
@@ -2,7 +2,13 @@
 
     namespace App\interfaces;
 
+    use App\classes\utility\containers\ErrorsContainer;
+    use App\classes\utility\containers\FormsWithData;
+
     interface InspectorInterface
     {
-
+        public function conductInspection();
+        public function setForms(FormsWithData $forms);
+        public function setModel(?ExistenceInterface $model);
+        public function setContainer(ErrorsContainer $container);
     }
