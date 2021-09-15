2e9f32e8b1ca413621ffdcfd6d6928af06ae7a5a
0kkama <mydeadtime@gmail.com>
2021-09-16 01:21:00 +0300

    Controller Categories completed

--- App/classes/controllers/Categories.php
+++ App/classes/controllers/Categories.php
@@ -3,6 +3,7 @@
     namespace App\classes\controllers;
 
     use App\classes\abstract\controllers\Controller;
+    use App\classes\models\ViewArticle;
     use App\classes\utility\containers\CategoriesList;
     use App\classes\models\Categories as CatModel;
     use App\classes\utility\View;
@@ -11,13 +12,49 @@
     {
         protected CategoriesList $list;
         protected string $title = 'Категории статей';
+        protected string $action;
+        protected array $articles;
+        protected CatModel $chosenCat;
 
         public function __construct(array $params, View $templateEngine)
         {
             ($this->list = new CategoriesList())->addArray(CatModel::getAllBy('status', '1'));
             parent::__construct($params, $templateEngine);
-            $this->content = $this->page->assign('categories', $this->list)->render('admin/categories');
+            $wrap = 'categories';
+            $object = $this->list;
+            $pattern = 'admin/categories';
+
+            if ( !empty($this->params['action']) ) {
+//                $this->action = $params['action'];
+                if ($this->choose()) {
+                    $this->title = $this->chosenCat->getTitle();
+                    $wrap = 'news';
+                    $object = $this->articles;
+                    $pattern = 'index';
+                } else {
+                    Error::deadend(404);
+                }
+//                $this->content = $this->page->assign('categories', $this->list)->render('admin/categories');
+            }
+                $this->content = $this->page->assign($wrap, $object)->render($pattern);
         }
+
+        protected function choose(): bool
+        {
+            if ($this->list->checkCategoryInBy('url', $this->params['action'])) {
+                $this->chosenCat = $this->list->getCategoryByIndex();
+                if(isset($this->chosenCat)) {
+                    $this->articles = ViewArticle::getAllBy('category', $this->chosenCat->getTitle());
+                    return true;
+                }
+            }
+            return false;
+        }
+//        protected function makeContent()
+//        {
+//            $this->content = $this->page->assign($wrap, $object)->render($pattern);
+//
+//        }
 
 
     }
