8404715673a51ad4b0e1166e6d237396ad1f2f7e
0kkama <mydeadtime@gmail.com>
2021-09-20 20:00:27 +0300

    Controllers articles/Read and articles/Create complited

--- App/classes/controllers/article/Read.php
+++ App/classes/controllers/article/Read.php
@@ -3,44 +3,25 @@
     namespace App\classes\controllers\article;
 
     use App\classes\abstract\controllers\Controller;
-    use App\classes\abstract\models\AbstractView;
-    use App\classes\models\ViewAllArticles;
+    use App\classes\abstract\models\ViewArticle;
+    use App\classes\models\view\ViewNotArchivedArticles;
+    use App\classes\utility\ArticleRepresentationzzz as Representation;
     use App\classes\utility\View;
-    use App\classes\utility\ArticleRepresentation as Representation;
 
     class Read extends Controller
     {
-//        protected AbstractView $viewArt;
-        protected ViewAllArticles $viewArt;
+        protected ViewArticle $viewArt;
         protected Representation $representation;
-
 
         public function __construct(array $params, View $templateEngine)
         {
             parent::__construct($params, $templateEngine);
             $this->id = $this->params['id'];
             $this->representation = new Representation();
+            $this->viewArt = $this->representation->readArticle($this->id, new ViewNotArchivedArticles());
 
-
-
-            $this->viewArt = ViewAllArticles::findOneBy('id', $this->id);
-
-//          статьи не существует
-//            статья сщуествует и открыта
-            //  статья есть, но на модерации
-//            статья есть, но она в
-
-            if ($this->user->exist()) {
-                if ($this->viewArt->getUserId() === $this->user->getId()) {
-                    $this->title = $this->viewArt->getTitle();
-                    $this->content = $this->page->assign('article', $value)->render('articles/article');
-                }
-            }
-
-
-
-
+            $this->title = $this->viewArt->getTitle();
+            $this->content = $this->page->assign('article', $this->viewArt)->render('articles/article');
         }
-
 
     }
